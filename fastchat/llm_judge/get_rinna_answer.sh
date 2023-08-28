c77802b105f4a45f1287c3a7de05302dd98defea
YukinoWan <zhen@saffron4.lang-private.kuee.kyoto-u.ac.jp>
2023-08-28 11:42:44 +0900

    remove useless file

--- fastchat/llm_judge/get_rinna_answer.sh
+++ fastchat/llm_judge/get_rinna_answer.sh
@@ -2,6 +2,6 @@ python gen_model_answer.py \
     --base_model "rinna/japanese-gpt-neox-3.6b-instruction-ppo"\
     --model_id rinna-3.6b-ppo\
     --with_prompt \
-    --gpus 0 \
+    --gpus 1 \
     --max_new_tokens 2048 \
     --benchmark jp_bench \
