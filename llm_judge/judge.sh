eec2be8b4b7005dd0f86b45f4ea5c61d07d07882
YukinoWan <Zhen_Wan@outlook.com>
2023-11-16 00:28:27 +0900

    update llmjp eval

--- llm_judge/judge.sh
+++ llm_judge/judge.sh
@@ -1,4 +1,4 @@
-OPENAI_API_KEY=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 python -B gen_judgment.py \
+OPENAI_API_KEY= python -B gen_judgment.py \
     --bench-name "jp_bench" \
     --mode pairwise-baseline  \
     --baseline-model gpt-3.5-davinci \
