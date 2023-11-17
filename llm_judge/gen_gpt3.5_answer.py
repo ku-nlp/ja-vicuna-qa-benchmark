9130ddcd9e765e68626fb1a9d72d97a579f6e485
YukinoWan <Zhen_Wan@outlook.com>
2023-11-17 17:54:16 +0900

    delete more useless files

--- llm_judge/gen_gpt3.5_answer.py
+++ llm_judge/gen_gpt3.5_answer.py
@@ -6,9 +6,48 @@ from tqdm import tqdm
 import sys
 import time
 import shortuuid
-#use following codes to activate azure:
 
-#use openai api with group PRISM
+
+class GPT3_Demo(object):
+    def __init__(self, engine, temperature, max_tokens, top_p, frequency_penalty, presence_penalty, best_of, logprobs):
+        self.engine = engine
+        self.temperature = temperature
+        self.max_tokens = max_tokens
+        self.top_p = top_p
+        self.frequency_penalty = frequency_penalty
+        self.presence_penalty = presence_penalty
+        self.best_of = best_of
+        self.logprobs = logprobs
+
+    def get_multiple_sample(self, prompt_list: List[str]):
+        response = openai.Completion.create(
+            engine=self.engine,
+            prompt=prompt_list,
+            temperature=self.temperature,
+            max_tokens=self.max_tokens,
+            top_p=self.top_p,
+            frequency_penalty=self.frequency_penalty,
+            presence_penalty=self.presence_penalty,
+            best_of=self.best_of,
+            logprobs=self.logprobs
+        )
+        results = [choice.text for choice in response.choices]
+        return results
+
+def run_gpt3(prompt_list):
+    demo = GPT3_Demo(
+        engine="text-davinci-003",  # text-davinci-003: best, text-ada-001: lowest price
+        temperature=0,  # control randomness: lowring results in less random completion (0 ~ 1.0)
+        max_tokens=8,  # max number of tokens to generate (1 ~ 4,000)
+        top_p=1,  # control diversity (0 ~ 1.0)
+        frequency_penalty=0,  # how to penalize new tokens based on their existing frequency (0 ~ 2.0)
+        presence_penalty=0,  # 这个是对于词是否已经出现过的惩罚，文档上说这个值调高可以增大谈论新topic的概率 (0 ~ 2.0)
+        best_of=3,  # 这个是说从多少个里选最好的，如果这里是10，就会生成10个然后选最好的，但是这样会更贵(1 ~ 20)
+        logprobs=1
+    )
+    results = demo.get_multiple_sample(prompt_list)
+    return results
+
 class Chat_Demo(object):
     def __init__(self, model, user_system, temperature, max_tokens, top_p, frequency_penalty, presence_penalty, n, stream, stop, logit_bias):
         self.model = model
@@ -79,9 +118,9 @@ if __name__ == '__main__':
         examples= instruction_list
     results = []
     for index, example in tqdm(enumerate(examples)):
-        response=run_chatgpt(example)
-        results.append({"question_id":question[index]["question_id"],"answer_id":shortuuid.uuid(),"model_id":"gpt-4" ,"choices":[{"index": 0, "turns": [response]}],"tstamp": time.time(),})    
-    predictions_file = "./data/jp_bench/model_answer/GPT-4_reference.jsonl"
+        response=run_gpt3(example)
+        results.append({"question_id":question[index]["question_id"],"answer_id":shortuuid.uuid(),"model_id":"gpt-3.5-davinci" ,"choices":[{"index": 0, "turns": [response]}],"tstamp": time.time(),})    
+    predictions_file = "./data/jp_bench/model_answer/gpt-3.5-davinci.jsonl"
     dirname = os.path.dirname(predictions_file)
     os.makedirs(dirname,exist_ok=True)
     with open(predictions_file, 'w') as f:
