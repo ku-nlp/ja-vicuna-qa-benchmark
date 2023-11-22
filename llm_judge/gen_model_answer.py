2c5afcb2b569fc1f02bff6cd5c6ba023d79e0e0b
Hirokazu Kiyomaru <h.kiyomaru@gmail.com>
2023-11-22 20:00:19 +0900

    load model configurations from file

--- llm_judge/gen_model_answer.py
+++ llm_judge/gen_model_answer.py
@@ -8,11 +8,7 @@ from tqdm import tqdm
 
 import numpy as np
 import torch
-from transformers import (
-    GenerationConfig,
-    AutoModelForCausalLM,
-    AutoTokenizer,
-)
+from transformers import AutoModelForCausalLM, AutoTokenizer
 from peft import PeftModel
 
 
@@ -29,126 +25,45 @@ default_temperature_config = {
 }
 
 
-def generate_response(input_text, tokenizer, model, temperature, max_new_tokens, args):
-    if "llm-jp" in args.base_model or (args.lora_model and "llm-jp" in args.lora_model):
-        input_text = "{instruction} ### 回答：".format_map({"instruction": input_text})
+def generate_response(
+    input_text, model, tokenizer, generation_config=None, special_token_map=None
+):
+    """Generate a response from the model given an input text.
 
-        inputs = tokenizer(
-            input_text, return_tensors="pt", add_special_tokens=False
-        ).to(model.device)
-        input_token_ids = inputs["input_ids"]
+    Args:
+        input_text (str): Input text.
+        model (transformers.PreTrainedModel): Model.
+        tokenizer (transformers.PreTrainedTokenizer): Tokenizer.
+        generation_config (dict): Generation config.
+        special_token_map (dict): Special token map. This is used to replace special tokens in the output.
+    """
+    inputs = tokenizer(input_text, return_tensors="pt", add_special_tokens=False).to(
+        model.device
+    )
+    input_token_ids = inputs["input_ids"]
 
+    if generation_config is None:
+        generation_config = {}
+
+    with torch.no_grad():
         output_token_ids = model.generate(
             **inputs,
-            top_p=0.9,
-            temperature=temperature,
-            do_sample=True,
-            max_new_tokens=max_new_tokens,
+            **generation_config,
             pad_token_id=tokenizer.pad_token_id,
             bos_token_id=tokenizer.bos_token_id,
             eos_token_id=tokenizer.eos_token_id,
         )[0]
-        output_token_ids = output_token_ids[input_token_ids.size(1) :]
-        output = tokenizer.decode(output_token_ids.tolist(), skip_special_tokens=True)
-        return output
-    elif "rinna" in args.base_model:
-        input_text = "ユーザー: {instruction}<NL>システム: ".format_map(
-            {"instruction": input_text}
-        )
-
-        input_token_ids = tokenizer.encode(
-            input_text, add_special_tokens=False, return_tensors="pt"
-        )
-        output_token_ids = model.generate(
-            input_token_ids.to(model.device),
-            do_sample=True,
-            max_new_tokens=max_new_tokens,
-            temperature=temperature,
-            repetition_penalty=1.1,
-            pad_token_id=tokenizer.pad_token_id,
-            bos_token_id=tokenizer.bos_token_id,
-            eos_token_id=tokenizer.eos_token_id,
-        )[0]
-        output_token_ids = output_token_ids[input_token_ids.size(1) :]
-        output = tokenizer.decode(output_token_ids.tolist(), skip_special_tokens=True)
-        output = output.replace("<NL>", "\n")
-        return output
-    elif "elyza" in args.base_model:
-        B_INST, E_INST = "[INST]", "[/INST]"
-        B_SYS, E_SYS = "<<SYS>>\n", "\n<</SYS>>\n\n"
-        DEFAULT_SYSTEM_PROMPT = "あなたは誠実で優秀な日本人のアシスタントです。"
-
-        prompt = "{bos_token}{b_inst} {system}{prompt} {e_inst} ".format(
-            bos_token=tokenizer.bos_token,
-            b_inst=B_INST,
-            system=f"{B_SYS}{DEFAULT_SYSTEM_PROMPT}{E_SYS}",
-            prompt=input_text,
-            e_inst=E_INST,
-        )
-
-        input_token_ids = tokenizer.encode(
-            prompt, add_special_tokens=False, return_tensors="pt"
-        )
-
-        output_token_ids = model.generate(
-            input_token_ids.to(model.device),
-            max_new_tokens=max_new_tokens,
-            pad_token_id=tokenizer.pad_token_id,
-            eos_token_id=tokenizer.eos_token_id,
-        )
-
-        output = tokenizer.decode(
-            output_token_ids.tolist()[0][input_token_ids.size(1) :],
-            skip_special_tokens=True,
-        )
-        return output
-
-    elif "llama" in args.base_model:
-        input_text = "以下にあるタスクの指示を示します。示された指示に適切に従うように回答を埋めてください。### 指示：\n\n{instruction}\n\n### 回答：\n\n".format_map(
-            {"instruction": input_text}
-        )
-        inputs = tokenizer(input_text, return_tensors="pt")
-        input_ids = inputs["input_ids"].to(device)
-        generation_config = GenerationConfig(
-            temperature=temperature,
-            top_p=0.75,
-            top_k=40,
-            num_beams=4,
-            no_repeat_ngram_size=3,
-        )
-
-        with torch.no_grad():
-            output_token_ids = model.generate(
-                input_ids=input_ids,
-                generation_config=generation_config,
-                return_dict_in_generate=True,
-                output_scores=True,
-                max_new_tokens=max_new_tokens,
-            )
-        s = output_token_ids.sequences[0]
-        output = tokenizer.decode(s)
-        output = output.split("### Response：")[1].strip()
-        output = output.split("\n\n")[0].strip()
-        return output
+    output_token_ids = output_token_ids[input_token_ids.size(1) :]
+    output = tokenizer.decode(output_token_ids.tolist(), skip_special_tokens=True)
+    if special_token_map:
+        for k, v in special_token_map.items():
+            output = output.replace(k, v)
+    return output
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("--base_model", default=None, type=str, required=True)
-    parser.add_argument(
-        "--lora_model",
-        default=None,
-        type=str,
-        help="If None, perform inference on the base model",
-    )
-    parser.add_argument("--model_id", default=None, type=str, help="name of the model")
-    parser.add_argument(
-        "--max_new_tokens", default=512, type=int, help="number of generated tokens"
-    )
-    parser.add_argument(
-        "--temperature", default=None, type=int, help="generation temperature"
-    )
-    parser.add_argument("--tokenizer_path", default=None, type=str)
+    parser.add_argument("--config", type=str, required=True, help="config file")
     parser.add_argument(
         "--benchmark",
         default="jp_bench",
@@ -160,9 +75,10 @@ if __name__ == "__main__":
     )
     args = parser.parse_args()
 
-    random.seed(args.seed)
-    np.random.seed(args.seed)
-    torch.manual_seed(args.seed)
+    seed = args.seed
+    random.seed(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
 
     if torch.cuda.is_available():
         device = "cuda"
@@ -171,17 +87,26 @@ if __name__ == "__main__":
         device = "cpu"
         torch_dtype = torch.float32
 
-    tokenizer_path = args.tokenizer_path or args.lora_model or args.base_model
-    tokenizer = AutoTokenizer.from_pretrained(tokenizer_path)
+    with open(args.config, "r") as f:
+        config = json.load(f)
 
     print("loading model")
+    model_name_or_path = config["model_name_or_path"]
     model = AutoModelForCausalLM.from_pretrained(
-        args.base_model, device_map="auto", torch_dtype=torch_dtype
+        model_name_or_path, device_map="auto", torch_dtype=torch_dtype
     )
-    if args.lora_model:
+    lora_model_name_or_path = config.get("lora_model_name_or_path")
+    if lora_model_name_or_path:
         print("loading peft model")
-        model = PeftModel.from_pretrained(model, args.lora_model)
+        model = PeftModel.from_pretrained(model, lora_model_name_or_path)
     model.eval()
+
+    tokenizer_name_or_path = (
+        config.get("tokenizer_name_or_path")
+        or lora_model_name_or_path
+        or model_name_or_path
+    )
+    tokenizer = AutoTokenizer.from_pretrained(tokenizer_name_or_path)
 
     # test data
     print("loading data")
@@ -192,17 +117,30 @@ if __name__ == "__main__":
             questions.append(json.loads(line))
 
     print("Start inference.")
+    model_id = config["model_id"]
+    prompt_template = config["prompt_template"]
+    if "{instruction}" not in prompt_template:
+        raise ValueError("prompt_template must contain {instruction}")
+    special_token_map = config.get("special_token_map", {})
     results = []
     for index, question in tqdm(enumerate(questions)):
+        generation_config = config.get("generation_config", {})
+        if (
+            "temperature" not in generation_config
+            or generation_config["temperature"] is None
+        ):
+            generation_config["temperature"] = default_temperature_config[
+                question["category"]
+            ]
+
         instruction = question["turns"][0]
-        temperature = (
-            args.temperature or default_temperature_config[question["category"]]
+        output = generate_response(
+            input_text=prompt_template.format_map({"instruction": instruction}),
+            model=model,
+            tokenizer=tokenizer,
+            generation_config=generation_config,
+            special_token_map=special_token_map,
         )
-        max_new_tokens = args.max_new_tokens
-        with torch.no_grad():
-            output = generate_response(
-                instruction, tokenizer, model, temperature, max_new_tokens, args
-            )
 
         print(f"======={index}=======")
         print(f"Input: {instruction}\n")
@@ -211,14 +149,14 @@ if __name__ == "__main__":
             {
                 "question_id": int(question["question_id"]),
                 "answer_id": shortuuid.uuid(),
-                "model_id": args.model_id,
+                "model_id": model_id,
                 "choices": [{"index": 0, "turns": [output]}],
                 "tstamp": time.time(),
             }
         )
 
     predictions_file = "./data/{}/model_answer/{}.jsonl".format(
-        args.benchmark, args.model_id
+        args.benchmark, model_id
     )
     dirname = os.path.dirname(predictions_file)
     os.makedirs(dirname, exist_ok=True)
