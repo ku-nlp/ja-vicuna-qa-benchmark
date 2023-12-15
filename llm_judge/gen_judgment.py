3e1dedb7ec934a4b82051ecc752e344e3c12711f
Hirokazu Kiyomaru <h.kiyomaru@gmail.com>
2023-12-15 15:22:02 +0900

    save leaderboard

--- llm_judge/gen_judgment.py
+++ llm_judge/gen_judgment.py
@@ -46,11 +46,11 @@ def make_match_single(
                 ref_answer = None
             matches.append(
                 MatchSingle(
-                    dict(question),
-                    model,
-                    answer,
-                    judge,
-                    ref_answer,
+                    question=question,
+                    model=model,
+                    answer=answer,
+                    judge=judge,
+                    ref_answer=ref_answer,
                 )
             )
         yield model, matches
@@ -82,12 +82,12 @@ def make_match_pairwise(
                 ref_answer = None
             matches.append(
                 MatchPair(
-                    dict(question),
-                    model_1,
-                    model_2,
-                    answer_1,
-                    answer_2,
-                    judge,
+                    question=question,
+                    model_1=model_1,
+                    model_2=model_2,
+                    answer_1=answer_1,
+                    answer_2=answer_2,
+                    judge=judge,
                     ref_answer=ref_answer,
                 )
             )
@@ -167,6 +167,11 @@ if __name__ == "__main__":
         import wandb
 
         wandb.login()
+        if args.mode != "pairwise-baseline":
+            logger.warning(
+                "Leaderboard is only available in pairwise-baseline mode. "
+                "Only raw outputs will be logged."
+            )
 
     logger.info("Load questions")
     questions = load_questions(str(QUESTION_FILE))
@@ -269,6 +274,43 @@ if __name__ == "__main__":
                 fout.write(json.dumps(result, ensure_ascii=False) + "\n")
 
         if args.wandb:
-            run = wandb.init(name=f"{args.mode}/{match_id}", reinit=True)
-            raw_results_table = wandb.Table(dataframe=pd.DataFrame(results))
-            run.log({"Raw Results": raw_results_table})
+            project = f"ja-vicuna-qa-benchmark|{args.mode}"
+            if args.mode == "pairwise-baseline":
+                project += f"|{args.baseline_model}"
+            run = wandb.init(project=project, name=match_id, reinit=True)
+
+            if args.mode == "pairwise-baseline":
+                match = matches[0]
+                if args.baseline_model == match.model_1:
+                    model = match.model_2
+                    baseline_model = match.model_1
+                else:
+                    model = match.model_1
+                    baseline_model = match.model_2
+
+                num_win = 0
+                num_tie = 0
+                for result in results:
+                    if (
+                        result["g1_winner"] == "tie"
+                        or result["g1_winner"] != result["g2_winner"]
+                    ):
+                        num_tie += 1
+                    elif result["g1_winner"] == "model_1":
+                        num_win += 1
+                win_rate = num_win / len(results)
+                adjusted_win_rate = (num_win + 0.5 * num_tie) / len(results)
+
+                leaderboard_table = wandb.Table(
+                    columns=[
+                        "model",
+                        "baseline_model",
+                        "win_rate",
+                        "adjusted_win_rate",
+                    ],
+                    data=[[model, baseline_model, win_rate, adjusted_win_rate]],
+                )
+                run.log({"leaderboard": leaderboard_table})
+
+            outputs_table = wandb.Table(dataframe=pd.DataFrame(results))
+            run.log({"outputs": outputs_table})
