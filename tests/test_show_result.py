25694b135c246327aaa91b1857cda4e3a6d2f0cf
Hirokazu Kiyomaru <h.kiyomaru@gmail.com>
2023-12-28 10:42:30 +0900

    test calculate_win_rate()

--- tests/test_show_result.py
+++ tests/test_show_result.py
@@ -1,6 +1,6 @@
 import unittest
 
-from llm_judge.show_result import calculate_average_score
+from llm_judge.show_result import calculate_average_score, calculate_win_rate
 
 
 class TestCalculateAverageScore(unittest.TestCase):
@@ -10,3 +10,39 @@ class TestCalculateAverageScore(unittest.TestCase):
 
         results = [{"score": 1}, {"score": 2}, {"score": 3}, {"score": 4}]
         self.assertEqual(calculate_average_score(results), 2.5)
+
+
+class TestCalculateWinRate(unittest.TestCase):
+    def test_calculate_win_rate(self):
+        results = [
+            {"g1_winner": "model_1", "g2_winner": "model_1"},
+        ]
+        self.assertEqual(
+            calculate_win_rate(results),
+            {
+                "model_1": {"win_rate": 1.0, "adjusted_win_rate": 1.0},
+                "model_2": {"win_rate": 0.0, "adjusted_win_rate": 0.0},
+            },
+        )
+
+        results = [
+            {"g1_winner": "model_1", "g2_winner": "model_2"},
+        ]
+        self.assertEqual(
+            calculate_win_rate(results),
+            {
+                "model_1": {"win_rate": 0.0, "adjusted_win_rate": 0.5},
+                "model_2": {"win_rate": 0.0, "adjusted_win_rate": 0.5},
+            },
+        )
+
+        results = [
+            {"g1_winner": "model_1", "g2_winner": "tie"},
+        ]
+        self.assertEqual(
+            calculate_win_rate(results),
+            {
+                "model_1": {"win_rate": 0.0, "adjusted_win_rate": 0.5},
+                "model_2": {"win_rate": 0.0, "adjusted_win_rate": 0.5},
+            },
+        )
